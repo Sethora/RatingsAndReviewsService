bf211aa4a74d65ff1da4a63895852c4409ff2445
Seth Lassen <sethmlassen@gmail.com>
2020-11-13 15:30:01 -0800

    Set up GET requests for both tables. Added two query functions

--- server/index.js
+++ server/index.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3321;
 const path = require('path')
+const db = require('../database')
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/products/:id/users', (req, res) => {
+  db.getAllUsers((users) => {
+    res.json(users);
+  })
+})
+
+app.get('/api/products/:id/reviews', (req, res) => {
+  db.getAllReviews((reviews) => {
+    res.json(reviews);
+  })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
