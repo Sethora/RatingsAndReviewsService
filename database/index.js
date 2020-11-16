c7814c092266ba4cc1f04aa656db27df3f2f4ef3
Seth Lassen <sethmlassen@gmail.com>
2020-11-16 11:58:46 -0800

    Deleted .gitkeep files

--- database/index.js
+++ database/index.js
@@ -3,19 +3,21 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-const getAllUsers = function(callback) {
+const getAllUsers = (callback) => {
   var queryString = "SELECT * from Users;";
   connection.query(queryString, (err, results) => {
     callback(results);
   })
 }
 
-const getAllReviews = function(callback) {
+const getAllReviews = (callback) => {
   var queryString = "SELECT * from Reviews;";
   connection.query(queryString, (err, results) => {
     callback(results);
   })
 }
+
+//const getAllStars
 
 module.exports = {
   getAllUsers,
